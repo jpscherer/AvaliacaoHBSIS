daca353fdede853160fcf5683038d713c70b2145
jpscherer <jean.patrick.scherer@gmail.com>
2018-03-11 20:35:59 -0300

    General Commit
    First commit with every filee.

---
+++ BibliotecaOnline/Controllers/BOOKsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace BibliotecaOnline.Controllers
+{
+    public class BOOKsController : Controller
+    {
+        private BibliotecaOnlineEntities db = new BibliotecaOnlineEntities();
+
+        // GET: BOOKs
+        public ActionResult Index()
+        {
+            return View(db.BOOKs.ToList());
+        }
+
+        // GET: BOOKs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BOOK bOOK = db.BOOKs.Find(id);
+            if (bOOK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bOOK);
+        }
+
+        // GET: BOOKs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BOOKs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,TITLE,DESCRIPTION,RELEASE_DATE")] BOOK bOOK)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BOOKs.Add(bOOK);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bOOK);
+        }
+
+        // GET: BOOKs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BOOK bOOK = db.BOOKs.Find(id);
+            if (bOOK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bOOK);
+        }
+
+        // POST: BOOKs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,TITLE,DESCRIPTION ,RELEASE_DATE")] BOOK bOOK)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bOOK).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bOOK);
+        }
+
+        // GET: BOOKs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BOOK bOOK = db.BOOKs.Find(id);
+            if (bOOK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bOOK);
+        }
+
+        // POST: BOOKs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BOOK bOOK = db.BOOKs.Find(id);
+            db.BOOKs.Remove(bOOK);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
